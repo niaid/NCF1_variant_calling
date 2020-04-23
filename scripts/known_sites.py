426be2a1bd24da8a5c174e5918c877b849b22932
Eric Karlins <karlinser@mail.nih.gov>
2020-04-23 11:31:01 -0400

    updated how known_sites.py is run to be flexible on known sites vcf

--- scripts/known_sites.py
+++ scripts/known_sites.py
@@ -5,6 +5,9 @@ from snakemake.shell import shell
 known = snakemake.input.get("known", "")
 if known:
     known = "--dbsnp " + known
+sites_vcf = snakemake.input.get("known_sites", "")
+if sites_vcf:
+    sites_vcf = "--alleles " + sites_vcf
 
 extra = snakemake.params.get("extra", "")
 java_opts = snakemake.params.get("java_opts", "")
@@ -15,7 +18,7 @@ bams = list(map("-I {}".format, bams))
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 shell(
-    "gatk3 {java_opts} -T HaplotypeCaller {extra} "
+    "gatk3 {java_opts} -T HaplotypeCaller {extra} {sites_vcf} "
     "-R {snakemake.input.ref} {bams} "
     "-o {snakemake.output.vcf} {known} {log}"
 )
